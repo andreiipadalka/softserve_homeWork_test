1cabba07f05bed413b8273965e93c5ff7b4ad33e
Andreii <an9reii@gmail.com>
2019-08-06 00:04:57 +0300

    ver 2

---
+++ v1/v1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            Console.WriteLine($"переменая а = {a}");
+            Console.WriteLine($"переменая а = {b}");
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"переменая а = {a}");
+            Console.WriteLine($"переменая а = {b}");
+        }
+    }
+}
---
+++ v2/v2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            Console.WriteLine($"переменая а = {a}");
+            Console.WriteLine($"переменая а = {b}");
+            a = a + b;
+            b = b - a;
+            b = -b;
+            a = a - b;
+            Console.WriteLine($"переменая а = {a}");
+            Console.WriteLine($"переменая а = {b}");
+        }
+    }
+}
